35968f2891f9ccc0e33c24215dec955433d35de4
atpriyavelmurugan <priyavelmurugan24_bcs28@mepcoeng.ac.in>
2026-07-27 11:59:11 +0530

    Create alterbits.c

---
+++ ex2/alterbits.c
@@ -0,0 +1,15 @@
+bool hasAlternatingBits(int n) {
+    int lastBit = n % 2;
+    n = n / 2; 
+    
+    while (n > 0) {
+        int currentBit = n % 2;
+        if (currentBit == lastBit) {
+            return false;
+        }
+        lastBit = currentBit;
+        n = n / 2;
+    }
+    
+    return true;
+}
