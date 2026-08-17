2c2e6c2173399efeeedc3b0123a8c3b5338f4caa
atpriyavelmurugan <priyavelmurugan24_bcs28@mepcoeng.ac.in>
2026-08-17 11:45:09 +0530

    Create 641.c

---
+++ ex3/641.c
@@ -0,0 +1,15 @@
+int hammingDistance(int x, int y) {
+   
+    int diff = x ^ y;
+    int count = 0;
+    
+    
+    while (diff > 0) {
+        
+        count += diff & 1;
+       
+        diff >>= 1; 
+    }
+    
+    return count;
+}
